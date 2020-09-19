0192011397f0a999a874d9d350a42e1ce4d83262
Luis Felipe Paris <lfparis@gmail.com>
2020-09-19 20:36:00 +0100

    updated download url

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author="Luis Felipe Paris",
     author_email="lfparis@gmail.com",
     url="https://github.com/lfparis/airbase",
-    download_url="https://github.com/lfparis/airbase/archive/0.0.1.tar.gz",
+    download_url="https://github.com/lfparis/airbase/archive/0.0.1a.tar.gz",
     version="0.0.1a",
     install_requires=["aiohttp", "pandas"],
     python_requires="!=2.7.*, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.6.*",  # noqa: E501
