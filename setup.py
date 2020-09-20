053fe8c5ef5d44ec67aa9a6028c1ba7eebc39f00
Luis Felipe Paris <lfparis@gmail.com>
2020-09-20 10:53:02 +0100

    Added utils to setup

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name="airtable-async",
-    packages=["airbase"],
+    packages=["airbase", "airbase.utils"],
     description="An asynchronous Python API Wrapper for the Airtable API",
     author="Luis Felipe Paris",
     author_email="lfparis@gmail.com",
     url="https://github.com/lfparis/airbase",
-    download_url="https://github.com/lfparis/airbase/archive/0.0.1.a.tar.gz",
-    version="0.0.1.a",
+    download_url="https://github.com/lfparis/airbase/archive/0.0.1.b.tar.gz",
+    version="0.0.1.b",
     install_requires=["aiohttp", "pandas"],
     python_requires="!=2.7.*, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.6.*",  # noqa: E501
     keywords=["airtable", "api", "async", "async.io"],
