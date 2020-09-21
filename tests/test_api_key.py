8b46c3b24f6f43381bb7d0097761db563713c44a
Luis Felipe Paris <lfparis@gmail.com>
2020-09-21 20:06:46 +0100

    passenv = LANG

--- tests/test_api_key.py
+++ tests/test_api_key.py
@@ -9,11 +9,8 @@ if sys.version_info[:2] < (3, 6):
     pass
 
 
-API_KEY = server.connect(os.environ["AIRTABLE_API_KEY"])
-
-
 @pytest.mark.asyncio
 async def test_api_key() -> None:
-    async with Airtable(api_key=API_KEY) as at:
+    async with Airtable(api_key=os.environ["AIRTABLE_API_KEY"]) as at:
         # Get all bases for a user
-        assert at.api_key == API_KEY
+        assert at.api_key == os.environ["AIRTABLE_API_KEY"]
