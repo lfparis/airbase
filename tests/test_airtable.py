9718ceb8878766926cb0c58af77cfb6b2e90ba92
Luis Felipe Paris <lfparis@gmail.com>
2020-09-21 19:43:36 +0100

    added pytest-asyncio

--- tests/test_airtable.py
+++ tests/test_airtable.py
@@ -9,9 +9,8 @@ if sys.version_info[:2] < (3, 6):
 
 @pytest.mark.asyncio
 async def test_airtable() -> None:
-    async with Airtable(api_key="a") as at:
-        assert at.api_key == "a"
+    async with Airtable() as at:
         # Get all bases for a user
-        # await at.get_bases()
-        # assert getattr(at, "bases", None)
-        # assert isinstance(at.bases[0], Base)
+        await at.get_bases()
+        assert getattr(at, "bases", None)
+        assert isinstance(at.bases[0], Base)
