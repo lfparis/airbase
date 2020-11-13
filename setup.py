7814b35690557b5412f09a182465e5bcc264ade8
Alex Rothberg <agrothberg@gmail.com>
2020-11-13 16:40:37 -0500

    Simplify python_requires line

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     url="https://github.com/lfparis/airbase",
     version="0.0.1b5",
     install_requires=["aiohttp", "pandas"],
-    python_requires="!=2.7.*, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.6.*",  # noqa: E501
+    python_requires=">=3.7",
     keywords=["airtable", "api", "async", "async.io"],
     license="The MIT License (MIT)",
     classifiers=[
